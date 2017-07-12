467957a8451beb1fd3ffbcc6bcbcee972811c214
Mbibby95 <mattyboysayshi@gmail.com>
2017-07-12 16:38:13 +0100

    Completed Cleaning of CSV

--- csv_clean.py
+++ csv_clean.py
@@ -2,13 +2,23 @@ import pandas as pd
 import numpy as np
 
 def main():
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help='Path to a CSV or HDF file')
-    args = parser.parse_args()
-    df = pd.read_csv(args.filename, index_col=0)
-    print(df.index.get_level_values(0).get_duplicates())
 
+    # Read in input file
+    df = pd.read_csv('input_data.csv', index_col=0)
+
+    # Remove duplicate indexes, keep the first instance
+    nodupes = df[~df.index.duplicated(keep='first')]
+
+    # Remove NaNs from dataset
+    nonulls = nodupes.dropna(subset=['q2', 'q4'])
+
+    # Read in q3 values
+    df_q3 = pd.read_csv('q3_column.csv', index_col=0)
+
+    # Merge q3 values with rest of dataframe
+    finaldf = nonulls.join(df_q3, how='left')
+
+    finaldf.to_csv('cleaned.csv')
 
 if __name__ == '__main__':
     main()
